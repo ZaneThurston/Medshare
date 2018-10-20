72016b4712dc06b42f9c41ccc30da3d530e19e95
Renan <renan.hespanhol@hotmail.com>
2018-10-20 17:59:30 -0300

    removendo migration

--- db/migrate/20181002183931_add_info_to_pin.rb
+++
@@ -1,7 +0,0 @@
-class AddInfoToPin < ActiveRecord::Migration[5.0]
-  def change
-    add_column :pins, :price, :integer
-    add_column :pins, :condition, :string
-    add_column :pins, :location, :string
-  end
-end
