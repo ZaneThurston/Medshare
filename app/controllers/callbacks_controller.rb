dbcb81c2eabb346d35f843832697aa57bcafdaff
Renan <renan.hespanhol@hotmail.com>
2018-10-20 17:36:08 -0300

    Removendo callback controller

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,2 @@
-class CallbacksController &lt; Devise::OmniauthCallbacksController def facebook @user = User.from_omniauth(request.env["omniauth.auth"]) if @user.persisted? sign_in_and_redirect @user, :event =&gt; :authentication
-    else
-      redirect_to new_user_registration_url
-    end
-  end
- 
-  def failure
-    redirect_to root_path
-  end
+class CallbacksController 
 end
