dd1f74b09aa0bdbfa2caf5e65bfd09b182f01169
PetersonMedeiros <peterson.santos@usp.br>
2018-10-20 19:24:37 -0300

    Login Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 	
-		 :omniauthable
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :pins
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
-      user.image = auth.info.image
+      #user.image = auth.info.image # assuming the user model has an image
     end
   end
 end
