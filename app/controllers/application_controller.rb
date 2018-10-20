68bfc609f52cc7ccea59913c5a2712836c15f27e
Renan <renan.hespanhol@hotmail.com>
2018-10-20 18:06:39 -0300

    comentando auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   puts "ApplicationController"
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   puts "ApplicationController end"
 end
